714ac1735a3bfa8b28cc5faaab548a10bac3cb4a
P1ST-0N <luckylionia@gmail.com>
2023-04-28 14:36:14 +0300

    added SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import Section from './Section/Section';
+import SearchBar from "./Searchbar/Searchbar";
 
 export default class App extends Component {
   state = {
+    imageName: '',
+  };
 
+  onFormSubmit = imageName => {
+    this.setState({ imageName });
   };
 
   render() {
     return (
       <>
+      <Section>
+        <SearchBar onSubmit={this.onFormSubmit} />
+      </Section>
+      <Section>
+        qwerewrq
+      </Section>
+
+      <ToastContainer autoClose={2000} />
       </>
     )
   }
